2a41e817b8f263d8a8b51f66df40e13d73a89c5d
SebasGarcia08 <segaracos@outlook.com>
2021-09-26 22:58:14 -0500

    Almost finished partitioning algorithm

--- fsm.py
+++ fsm.py
@@ -15,15 +15,15 @@ class FSM:
       Q (Tuple[str]): the set of states
       init_state (str): the initial state
     """
-    S: Tuple
-    R: Tuple
-    Q: Tuple
+    S: Tuple[str, ...]
+    R: Tuple[str, ...]
+    Q: Tuple[str, ...]
     init_state: str
     _transitions: List[List[Tuple[int, int]]] = field(default_factory=list)
     _inaccessible_states: List[int] = None
 
     def __post_init__(self) -> None:
-        for _ in range(len(self.Q)):
+        for i in range(len(self.Q)):
             self._transitions.append([None] * len(self.S))
 
     @property
@@ -58,7 +58,7 @@ class FSM:
             self.add_transition(src=state, dest=dest_state, stimulus=stimulus, output=output)
 
     def partition(self):
-        initial_partition: List[Set[int]] = []
+        initial_partition: List[List[int]] = []
         possible_outs: Dict[Tuple, Set[int]] = dict()
         self._inaccessible_states = self._get_inaccessible_states()
         print(f"{self._inaccessible_states = }")
@@ -71,12 +71,42 @@ class FSM:
             else:
                 possible_outs[outs].add(i)
         for block in possible_outs.values():
-            initial_partition.append(block)
+            initial_partition.append(list(block))
             print(list(map(lambda idx: self.Q[idx], block)))
         print(f"{initial_partition = }")
+        self._partition(initial_partition)
 
-    def _partition(self, prev_partition: List[Set[str]]):
-        pass
+    def _get_successors(self, partition: List[List[int]]) -> List[List[Tuple]]:
+        block_s_successors: List[List[Tuple]] = []
+        for block in partition:
+            block_successors: List[Tuple] = []
+            for state in block:
+                state_successors = tuple(self._transitions[state][i][0]
+                                         for i in range(len(self.S)))
+                block_successors.append(state_successors)
+            block_s_successors.append(block_successors)
+        return block_s_successors
+
+    def _partition(self, prev_partition: List[List[int]]):
+        block_s_successors = self._get_successors(prev_partition)
+        for successors_block in block_s_successors:
+            print(successors_block)
+            for block in successors_block:
+                print(tuple(map(lambda idx: self.Q[idx], block)), end=" ")
+
+        new_partition: Dict[Tuple, int] = dict()
+        for i_block in range(len(prev_partition)):
+            for i_successors in range(len(block_s_successors[i_block])):
+                s_successors = block_s_successors[i_block][i_successors]
+                prev_block_belongs = tuple(self._which_block(s, prev_partition) for s in s_successors)
+                #print(prev_partition[i_block][i_successors], prev_block_belongs)
+                #print()
+        print()
+
+    def _which_block(self, successor: int, partition: List[List[int]]) -> int:
+        for i, block in enumerate(partition):
+            if successor in block:
+                return i
 
     def _get_inaccessible_states(self) -> List[int]:
         start_idx = self.Q.index(self.init_state)
@@ -88,8 +118,6 @@ class FSM:
     def _get_accessible_states_from(self, src_idx: int, visited: Set[int]) -> None:
         to_visit = set(map(lambda x: x[0], self._transitions[src_idx]))
         visited.add(src_idx)
-        #print(f"{visited = }")
-        #print(f"{ to_visit = }")
         for to_vis in to_visit:
             if to_vis not in visited:
                 self._get_accessible_states_from(to_vis, visited)
